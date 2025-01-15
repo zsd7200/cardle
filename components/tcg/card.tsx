878eb548afb90864c79fd9437710db1335151457
zsd7200 <dunhamz2016@gmail.com>
2025-01-15 17:15:45 -0500

    better visuals for wins/losses, start adjusting for mobile screens

--- components/tcg/card.tsx
+++ components/tcg/card.tsx
@@ -6,6 +6,8 @@ import { compareTwoStrings } from 'string-similarity';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import Confetti from 'react-confetti';
+import Modal from 'react-modal';
+import Link from 'next/link';
 
 type CardProps = {
   data?: InnerCardData
@@ -16,9 +18,10 @@ export default function Card(props: CardProps | undefined = undefined) {
   const [monNames, setMonNames] = useState<Array<string>>(['Pokémon']);
   const [guessesRemaining, setGuessesRemaining] = useState<number>(3);
   const [guessesUsed, setGuessesUsed] = useState<number>(0);
-  const [winState, setWinState] = useState<Boolean>(false);
-  const [loseState, setLoseState] = useState<Boolean>(false);
+  const [winState, setWinState] = useState<boolean>(false);
+  const [loseState, setLoseState] = useState<boolean>(false);
   const [confettiPieces, setConfettiPieces] = useState<number>(200);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     let loadCardData = async () => {
@@ -75,6 +78,8 @@ export default function Card(props: CardProps | undefined = undefined) {
 
   let handleSubmit = (e: any) => {
     e.preventDefault();
+    if (e.target.mon.value.length <= 1) return;
+
     const answer = formatName(cardData.name);
     const guess = formatName(e.target.mon.value);
     const minAllowedPercent = 0.8;
@@ -84,6 +89,7 @@ export default function Card(props: CardProps | undefined = undefined) {
       const newGuessesUsed = guessesUsed + 1;
       setGuessesRemaining(newGuessesRemaining);
       setGuessesUsed(newGuessesUsed);
+      e.target.mon.value = null;
 
       if (newGuessesRemaining == 0) {
         return handleLoss();
@@ -95,12 +101,18 @@ export default function Card(props: CardProps | undefined = undefined) {
   };
 
   let handleWin = () => {
-    setWinState(true);
     setTimeout(() => { setConfettiPieces(0); }, 5000);
+    setWinState(true);
+    setIsModalOpen(true);
   }
 
   let handleLoss = () => {
     setLoseState(true);
+    setIsModalOpen(true);
+  }
+
+  let closeModal = () => {
+    setIsModalOpen(false);
   }
 
   let cardClasses = () => {
@@ -124,15 +136,19 @@ export default function Card(props: CardProps | undefined = undefined) {
     return base;
   }
 
+  let shareHandler = () => {
+    navigator.clipboard.writeText('Cardle: https://cardle.wtf/');
+  }
+
   return (
     <>
-      <div className="flex justify-center items-center gap-[50px]">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-[50px]">
         <div className="overflow-hidden">
-          <div className="overflow-hidden max-h-[700px] w-fit">
-            <img src={cardData.images.large ?? cardData.images.small} className={cardClasses()} id="card-image" />
+          <div className="overflow-hidden max-h-[700px] w-fit ">
+            <img src={cardData.images.large ?? cardData.images.small} className={cardClasses()} />
           </div>
         </div>
-        <div className="flex flex-col w-1/3 text-lg rounded-lg px-[15px] py-[10px] shadow-[0px_0px_13px_8px_rgba(128,_128,_128,_0.1)] bg-gray-700">
+        <div className="flex flex-col w-5/6 lg:w-1/2 xl:w-1/3 text-lg rounded-lg px-[15px] py-[10px] shadow-[0px_0px_13px_8px_rgba(128,_128,_128,_0.1)] bg-gray-700">
           <div className="flex flex-col pb-[15px]">
             <div className="flex justify-between">
               <span>Classification:</span>
@@ -175,7 +191,12 @@ export default function Card(props: CardProps | undefined = undefined) {
                   <option key={name} value={name}></option>
                 ))}
               </datalist>
-              <button type="submit" className="bg-purple-400 hover:bg-purple-600 active:bg-purple-800 text-white font-bold py-1 px-3 rounded transition">Guess</button>
+              <button 
+                type="submit" 
+                className="bg-purple-400 hover:bg-purple-600 active:bg-purple-800 text-white font-bold py-1 px-3 rounded transition" 
+                disabled={(winState || loseState)}>
+                  Guess
+              </button>
             </form>
             <div>
               <ul className="flex gap-[10px]">
@@ -188,9 +209,67 @@ export default function Card(props: CardProps | undefined = undefined) {
               </ul>
             </div>
           </div>
+          {
+            (winState || loseState) &&
+            <>
+              <hr className="h-px mt-5 mb-3 bg-purple-200 border-0 dark:bg-purple-400" />
+              <div className="flex justify-center align-center">
+                <span>The answer was <b>{cardData.name}</b>!</span>
+              </div>
+            </>
+          }
         </div>
       </div>
       {winState && <Confetti numberOfPieces={confettiPieces} />}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={{
+          overlay: {
+            background: 'rgba(0, 0, 0, 0.5)',
+          },
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            background: 'rgb(55 65 81)',
+            border: 'none',
+            boxShadow: '0px 0px 13px 8px rgba(128,128,128,0.3)'
+          },
+        }}
+      >
+        <>
+          {winState && 
+          <>
+            <h2 className="font-bold text-2xl text-center">Congratulations! 🎉</h2>
+            <span className="text-lg">Come back tomorrow for a new Daily Challenge!</span>
+          </>}
+          {loseState && 
+          <>
+            <h2 className="font-bold text-2xl text-center">😔 Better luck next time!</h2>
+          </>
+          }
+          <hr className="h-px mt-5 mb-5 bg-purple-200 border-0 dark:bg-purple-400" />
+          <div className="flex justify-around">
+            <Link 
+              href="/"
+              className="bg-purple-400 hover:bg-purple-600 active:bg-purple-800 text-white font-bold py-1 px-3 rounded transition w-2/5 text-center" 
+            >
+              Go Home
+            </Link>
+            <button 
+              type="button"
+              onClick={shareHandler} 
+              className="bg-purple-400 hover:bg-purple-600 active:bg-purple-800 text-white font-bold py-1 px-3 rounded transition w-2/5 text-center" 
+            >
+              Share
+            </button>
+          </div>
+        </>
+      </Modal>
     </>
   );
 }
