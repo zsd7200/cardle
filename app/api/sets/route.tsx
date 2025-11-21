7cfeafb850ee84cb0d191f1414c0ae4bf1abe176
Zack Dunham <dunhamz2016@gmail.com>
2025-11-21 11:13:26 -0500

    add explicit runtime for sets api

--- app/api/sets/route.tsx
+++ app/api/sets/route.tsx
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { getSetIDs } from "@/components/db/Set";
 
+export const runtime = 'nodejs';
+
 export async function GET() {
   try {
     const res = await getSetIDs();
